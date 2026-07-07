0a00f778be333289e27024aeb4e65be4d0285444
zaky <abozaky.net546@gmail.com>
2026-07-07 09:46:19 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,33 @@
-const CACHE_NAME = 'mtab3a-cache-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE_NAME = 'mtab3a-cache-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-maskable.svg'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.all(
+        ASSETS.map(url => cache.add(url).catch(err => {
+          console.warn('sw: failed to cache', url, err);
+        }))
+      )
+    )
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(res => res || caches.match('./index.html'))));
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(res => res || caches.match('./index.html'))
+    )
+  );
 });
