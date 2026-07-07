050b80f20429c315e25715775e9f34d29164679f
zaky <abozaky.net546@gmail.com>
2026-07-07 10:17:47 +0300

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,33 +1,17 @@
 const CACHE_NAME = 'mtab3a-cache-v2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-maskable.svg'];
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache =>
-      Promise.all(
-        ASSETS.map(url => cache.add(url).catch(err => {
-          console.warn('sw: failed to cache', url, err);
-        }))
-      )
-    )
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request).then(res => res || caches.match('./index.html'))
-    )
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(res => res || caches.match('./index.html'))));
 });
